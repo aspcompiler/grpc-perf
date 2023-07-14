30e3ed382d57be4c079af76dc473d51ea4460ccb
Li Chen <amazonli@amazon.com>
2023-07-14 12:52:38 -0700

    Move the directory for Rust

--- rust-tonic/server_streaming/build.rs
+++ rust-tonic/server_streaming/build.rs
@@ -6,6 +6,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn build_grpc() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .compile_well_known_types(true)
-        .compile(&["../protos/server_streaming.proto"], &["../protos"])?;
+        .compile(&["../../protos/server_streaming.proto"], &["../../protos"])?;
     Ok(())
 }
